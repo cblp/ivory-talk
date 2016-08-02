098c839c8c73561900a5678aa582f4027dddf34e
Yuriy Syrovetskiy <cblp@cblp.su>
2016-08-02 11:42:31 +0300

    [m] replace Pandoc with Hamlet

--- build.hs
+++ build.hs
@@ -1,22 +1,28 @@
 #!/usr/bin/env stack
--- stack --resolver=lts-6.10 runhaskell --package=shake
+-- stack --resolver=lts-6.10 runhaskell --package=shake --package=shakespeare
 {-# OPTIONS -Wall -Werror #-}
+{-# LANGUAGE OverloadedStrings #-}
 
+import qualified Data.Map                        as Map
 import           Development.Shake
 import           Development.Shake.FilePath
+import           Text.Blaze.Html.Renderer.String
+import           Text.Hamlet.Runtime
 
 main :: IO ()
 main = shakeArgs shakeOptions $ do
 
     want ["ivory-talk.html"]
 
-    "*.html" %> \html -> do
-        let md = html -<.> "md"
-        need [md, "build.hs"]
-        cmd "pandoc"  [ "--output=" ++ html
-                      , "--standalone"
-                      , "--to=revealjs"
-                      , "--variable=theme:serif"
-                      , "--variable=width:'100%'"
-                      , md
-                      ]
+    "*.html" %> \outFile -> do
+        let srcFile = outFile -<.> "hamlet"
+            layoutFile = "revealjs.hamlet"
+        need [srcFile, layoutFile, "build.hs"]
+        -- , "--to=revealjs"
+        -- , "--variable=theme:serif"
+        -- , "--variable=width:'100%'"
+        src <- liftIO $ readHamletTemplateFile defaultHamletSettings srcFile
+        slides <- liftIO $ renderHamletTemplate src mempty
+        layout <- liftIO $ readHamletTemplateFile defaultHamletSettings layoutFile
+        out <- liftIO $ renderHamletTemplate layout (Map.fromList [("slides", toHamletData slides)])
+        writeFile' outFile $ renderHtml out
