4af3c78757bb28865881466f740058ffdf35ab59
Yuriy Syrovetskiy <cblp@cblp.su>
2016-08-03 17:46:10 +0300

    [+] more about language-c-quote

--- code/languagec-hello.hs
+++ code/languagec-hello.hs
@@ -3,13 +3,33 @@
 import           Language.C.Quote.C
 import           System.Environment
 import           Text.PrettyPrint.Mainland
+-- import           Text.Show.Pretty
 
 main :: IO ()
 main = do
     msg:_ <- getArgs
-    putDoc $ ppr [cunit|
+    let code = [cunit|
         $esc:("#include <stdio.h>")
         int main() {
             puts($string:msg);
         }
     |]
+    putDoc $ ppr code
+    -- putStrLn $ ppShow code
+
+-- [ EscDef "#include <stdio.h>"
+-- , FuncDef
+--     (OldFunc
+--        (DeclSpec [] [] (Tint Nothing))
+--        (Id "main")
+--        (DeclRoot)
+--        []
+--        Nothing
+--        [ BlockStm
+--            (Exp
+--               (Just
+--                  (FnCall
+--                     (Var (Id "puts"))
+--                     [ Const
+--                         ( StringConst ["\"CoLaboratory:ruHaskell 2016\""]
+--                           "CoLaboratory:ruHaskell 2016" ) ] ) ) ) ] ) ]
